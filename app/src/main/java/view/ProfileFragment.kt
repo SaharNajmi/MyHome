0fca766fdcc013481c812a4b00606061d8785eb7
sahar <7899798-saharnajmi@users.noreply.gitlab.com>
2021-06-02 20:46:53 +0430

    loadingView-ProgressBar

--- app/src/main/java/view/ProfileFragment.kt
+++ app/src/main/java/view/ProfileFragment.kt
@@ -1,13 +1,13 @@
 package view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myhome.R
+import main.MyHomeFragment
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : MyHomeFragment() {
 
 
     override fun onCreateView(
@@ -16,5 +16,14 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // برای نشان دادن یا ندادن پروگرس بار فقط کافیه مقدار true false بدیم
+      setProgress(true)
+
+
     }
 }
